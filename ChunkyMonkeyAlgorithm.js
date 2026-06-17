16a749e71c35265e25fa92c50325813e55e8f90f
Flopchamp <alooharrison7@gmail.com>
2026-06-17 08:39:41 +0300

    fix ChunkyMonkeyAlgorithm.js to use slice instead of splice for chunking arrays

--- ChunkyMonkeyAlgorithm.js
+++ ChunkyMonkeyAlgorithm.js
@@ -1,7 +1,7 @@
 function chunkArrayInGroups(arr,num){
   const result = [];
   for(let i = 0;i < arr.length;i += num){
-    result.push(arr.splice(i,i+num));
+    result.push(arr.slice(i,i+num));
   }
   return result;
 }
