6c02b1e462c15c4a0e4fd4dd4c930b1178e42dec
Flopchamp <alooharrison7@gmail.com>
2026-07-27 11:45:07 +0300

    Refactor displayPlantsSet function and comment out localStorage code snippets

--- Plant Nursery Catalog.js
+++ Plant Nursery Catalog.js
@@ -74,3 +74,20 @@ const displayPlantsSet = () => {
 
 const plantsSet = displayPlantsSet();
 console.log(plantsSet);
+
+
+// const myTaskArr = [
+//   { task: "Walk the Dog", date: "22-04-2022" },
+//   { task: "Read some books", date: "02-11-2023" },
+//   { task: "Watch football", date: "10-08-2021" },
+// ];
+
+// localStorage.setItem("data", JSON.stringify(myTaskArr));
+
+// localStorage.removeItem("data");
+
+// const getTaskArr = localStorage.getItem("data")
+// console.log(getTaskArr)
+
+// const getTaskArrObj = JSON.parse(localStorage.getItem("data"));
+// console.log(getTaskArrObj);
