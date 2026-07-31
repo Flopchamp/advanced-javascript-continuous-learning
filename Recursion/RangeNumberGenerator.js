fb3dd48cb43c55a4c155c2f09db6ce35c40c8a3a
Flopchamp <alooharrison7@gmail.com>
2026-07-31 10:33:25 +0300

    add comments to clarify recursive logic in rangeOfNumbers function

--- Recursion/RangeNumberGenerator.js
+++ Recursion/RangeNumberGenerator.js
@@ -2,7 +2,9 @@ function rangeOfNumbers (startNum,endNum){
   if(startNum > endNum){
     return [];
   } else {
+    // Recursive case: generate the range of numbers from startNum to endNum
     const numbers = rangeOfNumbers(startNum + 1, endNum);
+    // Add the current startNum to the beginning of the array
     numbers.unshift(startNum);
     return numbers;
   }
