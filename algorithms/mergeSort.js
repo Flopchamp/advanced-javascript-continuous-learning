0ec324892895abcfef87ed34b8c3be2be400e6d4
Flopchamp <alooharrison7@gmail.com>
2026-08-06 10:04:20 +0300

    add merge sort implementation with core functionality

---
+++ algorithms/mergeSort.js
@@ -0,0 +1,24 @@
+function mergeSort(arr){
+    if(arr.length <= 1){
+        return arr
+    }
+    const mid = Math.floor(arr.length /2);
+    const left = mergeSort(arr.slice(0,mid));
+    const right = mergeSort(arr.slice(mid));
+
+    const Sorted =[];
+    let i =0;
+    let j =0;
+    while(i< left.length && j< right.length){
+        if(left[i] <= right[j]){
+            Sorted.push(left[i]);
+            i+=1;
+        }
+        else{
+            Sorted.push(right[j]);
+            j+=1;
+        }
+    }
+    return Sorted.concat(left.slice(i)).concat(right.slice(j));
+}
+console.log(mergeSort([5,4,3,2,1]))
